406e06082be723b9f703884f3d0c18ce73e0e30a
almavit <matveev@tut.by>
2019-11-25 21:10:00 +0300

    Выгрузить название кнопки

--- app/src/main/java/by/matfeev/lenovostart/ScanerActivity.java
+++ app/src/main/java/by/matfeev/lenovostart/ScanerActivity.java
@@ -559,7 +559,7 @@ public class ScanerActivity extends AppCompatActivity implements View.OnClickLis
                                         dialog.cancel();
                                     }
                                 })
-                        .setNegativeButton("_",
+                        .setNegativeButton("Выгрузить",
                                 new DialogInterface.OnClickListener() {
                                     public void onClick(DialogInterface dialog,
                                                         int id) {
