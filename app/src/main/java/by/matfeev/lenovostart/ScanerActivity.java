438ac1dfbe7532bc9a54f9962a5109daa2cd36d1
almavit <matveev@tut.by>
2019-11-25 20:39:49 +0300

    Load project

--- app/src/main/java/by/matfeev/lenovostart/ScanerActivity.java
+++ app/src/main/java/by/matfeev/lenovostart/ScanerActivity.java
@@ -559,7 +559,7 @@ public class ScanerActivity extends AppCompatActivity implements View.OnClickLis
                                         dialog.cancel();
                                     }
                                 })
-                        .setNegativeButton("Выгрузить",
+                        .setNegativeButton("",
                                 new DialogInterface.OnClickListener() {
                                     public void onClick(DialogInterface dialog,
                                                         int id) {
